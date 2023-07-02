ec4ae2b0df52292c7ab46e66c53b99d3e91a9a63
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-02 11:39:26 +0530

    Added fn to render notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note"
+import notes from "../notes";
 function App(){
   return (
     <div>
       <Header />
-      <Note />
+    {notes.map(note=><Note title={note.title} content={note.content}/>)}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function note(){
+
+
+function note(props){
     return (
     <div className="note">
-        <h1>This is the title</h1>
-        <p>This is the note content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 )
 }
