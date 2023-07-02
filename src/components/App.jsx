157e88665458cc92425173535c80bc63f69fe35c
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-02 11:46:45 +0530

    Added key for each notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ function App(){
   return (
     <div>
       <Header />
-    {notes.map(note=><Note title={note.title} content={note.content}/>)}
+    {notes.map(note=><Note key={note.key}keytitle={note.title} content={note.content} />)}
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -17,9 +17,5 @@ import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App/>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
