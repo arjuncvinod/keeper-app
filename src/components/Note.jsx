9f390ad400985b81ac10408aeacae5f651477766
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-09 23:15:44 +0530

    material ui icons added

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import DeleteIcon from "@mui/icons-material/Delete";
 function Note(props) {
 
     function handleClick(){
@@ -10,7 +10,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}><DeleteIcon /> </button>
     </div>
   );
 }
