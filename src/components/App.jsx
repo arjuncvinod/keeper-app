127b14f2a66a8fecea116320ccd345b0be8dd3a3
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-09 22:20:02 +0530

    dynamically renderd each note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,12 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      <Note key={1} title="Note title" content="Note content" />
+      
+    {notes.map((noteItem)=>{
+      return <Note key={1} title={noteItem.title} content={noteItem.content} />
+    })}
+
+      
       <Footer />
     </div>
   );
