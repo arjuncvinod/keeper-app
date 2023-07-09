0a411ef83f1594cbbd0ee19b1c4307822e6b3d99
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-09 19:28:48 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note"
-import notes from "../notes";
-function App(){
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
   return (
     <div>
       <Header />
-    {notes.map(note=><Note key={note.key}keytitle={note.title} content={note.content} />)}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
 }
+
 export default App;
